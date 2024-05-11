51a522992ca8a64e7c1066275b254aed528f9dc4
AptRoApt <rostislav07.02.05@mail.ru>
2024-05-12 01:20:24 +0300

    рабочая версия

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -21,13 +23,24 @@ type Parser struct {
 	scheduler gocron.Scheduler
 }
 
+type headerTransport struct {
+}
+
+func (ht headerTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	if req.Method == "POST" {
+		req.Header.Set("User-Agent", "StudyProject (contact: ampore@mail.ru)")
+	}
+	return http.DefaultTransport.RoundTrip(req)
+}
+
 func New(s *storage.Storage) Parser {
 	sr, err := gocron.NewScheduler()
 	if err != nil {
 		panic("Ошибка при создании планировщика.")
 	}
 	p := Parser{
-		client:    http.DefaultClient,
+		client:    &http.Client{Transport: headerTransport{}},
 		storage:   s,
 		scheduler: sr,
 	}
@@ -51,6 +64,7 @@ func New(s *storage.Storage) Parser {
 
 func (p *Parser) Start() {
 	p.GetGroups()
+	p.GetSchedule()
 	p.scheduler.Start()
 }
 
@@ -66,7 +80,7 @@ func (p *Parser) getElementsList(elementName string, data url.Values) ([]string,
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("Ошибка при запросе к реа")
+		return nil, fmt.Errorf("ошибка при запросе к реа")
 	}
 
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
@@ -86,7 +100,15 @@ func (p *Parser) getElementsList(elementName string, data url.Values) ([]string,
 	return result, nil
 }
 
-func (p *Parser) getFacultiesGroups(faculty string, data url.Values) ([]models.Group, error) {
+func (p *Parser) getFacultiesGroups(faculty string) ([]models.Group, error) {
+	data := url.Values{
+		"Faculty":      {"na"},
+		"Course":       {"na"},
+		"Type":         {"na"},
+		"ChangedNode":  {"na"},
+		"ChangedValue": {"na"},
+	}
+
 	data.Set("Faculty", faculty)
 	data.Set("ChangedNode", "Faculty")
 	data.Set("ChangeValue", faculty)
@@ -112,14 +134,15 @@ func (p *Parser) getFacultiesGroups(faculty string, data url.Values) ([]models.G
 			data.Set("ChangeValue", edType)
 
 			groups, err := p.getElementsList("Group", data)
+			courseNum, _ := strconv.Atoi(string(course[0]))
 			for _, group := range groups {
 				facultyGroups = append(
 					facultyGroups,
 					models.Group{
 						Name:          group,
-						Faculty:       models.Faculty{Name: faculty},
-						Course:        int(course[0]),
-						EducationType: models.EducationType{Name: edType},
+						Faculty:       p.storage.SaveAndGetFaculty(faculty),
+						Course:        courseNum,
+						EducationType: p.storage.SaveAndGetEducationType(edType),
 					},
 				)
 			}
@@ -133,18 +156,30 @@ func (p *Parser) getFacultiesGroups(faculty string, data url.Values) ([]models.G
 
 func (p *Parser) GetGroups() {
 	// Получаем список факультетов
-	data := url.Values{
-		"Faculty":      {"na"},
-		"Course":       {"na"},
-		"Type":         {"na"},
-		"ChangedNode":  {"na"},
-		"ChangedValue": {"na"},
-	}
-	faculties, err := p.getElementsList("Faculty", data)
+	resp, err := p.client.Get("https://rasp.rea.ru/Schedule/Navigator")
 	if err != nil {
-		log.Println(err)
 		return
 	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return
+	}
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return
+	}
+
+	options := doc.Find("select[name=\"Faculty\"] option").FilterFunction(func(i int, s *goquery.Selection) bool {
+		return i > 0
+	})
+
+	var faculties []string
+	options.Each(func(i int, s *goquery.Selection) {
+		faculties = append(faculties, s.Text())
+	})
+
 	// Параллельно собираем список групп
 	var groups []models.Group
 	var wg sync.WaitGroup
@@ -154,7 +189,7 @@ func (p *Parser) GetGroups() {
 		wg.Add(1)
 		go func(f string) {
 			defer wg.Done()
-			facultyGroups, err := p.getFacultiesGroups(f, data)
+			facultyGroups, err := p.getFacultiesGroups(f)
 			if err != nil {
 				log.Println(err)
 				return
@@ -167,23 +202,167 @@ func (p *Parser) GetGroups() {
 
 	wg.Wait()
 
-	err = p.storage.AddGroups(context.TODO(), groups)
-	if err != nil {
-		log.Println(err)
-		return
+	for _, faculty := range faculties {
+		facultyGroups, err := p.getFacultiesGroups(faculty)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		groups = append(groups, facultyGroups...)
 	}
+
+	p.storage.SaveOrUpdateGroups(context.TODO(), groups)
 }
 
-func (p *Parser) getScheduleForGroup(group string, weekNum int) {
+func (p *Parser) getLesson(group models.Group, date string, timeslot int) []models.Lesson {
+	var targetLink = fmt.Sprintf(
+		"https://rasp.rea.ru/Schedule/GetDetails?selection=%s&date=%s&timeSlot=%v",
+		strings.ToLower(group.Name),
+		date,
+		timeslot,
+	)
 
+	resp, err := p.client.Get(targetLink)
+	if err != nil {
+		//log somehow
+		return nil
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		//log
+		return nil
+	}
+
+	var lessons = make([]models.Lesson, 0, 2)
+
+	elements := doc.Find(".element-info-body")
+	withSubgroups := elements.Size() == 2
+
+	elements.Each(
+		func(i int, s *goquery.Selection) {
+			//Получаем дату
+			date, err := time.Parse("02.01.2006", "20.12.2024")
+			if err != nil {
+				//log
+				return
+			}
+
+			var isCommission bool
+			var lessonType string
+			// Получаем тип занятия и есть ли комиссия
+			s.Find("strong").Each(
+				func(i int, s *goquery.Selection) {
+					if i > 0 {
+						isCommission = true
+						return
+					}
+					lessonType = s.Text()
+				})
+			// Получаем номер подгруппы, если она есть
+			var subgroupNum int
+			if withSubgroups {
+				subgroupNum = i + 1
+			}
+			// Парсим кафедру
+			cathedraStartIndex := strings.LastIndex(s.Text(), "(") + 1
+			cathedraEndIndex := strings.LastIndex(s.Text(), ")")
+			cathedraName := s.Text()[cathedraStartIndex:cathedraEndIndex]
+			cathedra := p.storage.SaveAndGetCathedraByName(context.TODO(), cathedraName)
+			// И корпус
+			buildingIndex := strings.Index(s.Text(), "корпус") - 2
+			buildingNum, err := strconv.Atoi(s.Text()[buildingIndex : buildingIndex+1])
+			if err != nil {
+				//log
+				return
+			}
+			// И номер аудитории
+			delta := strings.Index(s.Text()[buildingIndex+25:], "\n")
+			room := s.Text()[buildingIndex+25 : buildingIndex+25+delta] // Там может быть Вебинар, так что это должна быть строка
+			// И айди пары.
+			id, err := strconv.Atoi(s.Find(".task-id-display").Text()[5:])
+			if err != nil {
+				//logrus
+				return
+			}
+
+			// Получаем учителей(я)
+			var teachers = make([]models.Teacher, 0, 1)
+			s.Find("a").Each(
+				func(i int, s *goquery.Selection) {
+					teachers = append(teachers, p.storage.SaveAndGetTeacher(
+						context.TODO(),
+						s.Text()[7:],
+						cathedra,
+					))
+				})
+
+			var lesson = models.Lesson{
+				Id:   id,
+				Date: date,
+				LessonTime: models.LessonTime{
+					Num: timeslot,
+				},
+				LessonType:   p.storage.GetLessonTypeByName(context.TODO(), lessonType),
+				Discipline:   p.storage.GetDisciplineByName(context.TODO(), s.Find("h5").Text()),
+				Room:         p.storage.SaveAndGetRoom(context.TODO(), buildingNum, room),
+				Teachers:     teachers,
+				Groups:       []models.Group{group},
+				SubgroupNum:  subgroupNum,
+				Cathedra:     cathedra,
+				IsCommission: isCommission,
+			}
+			lessons = append(lessons, lesson)
+		})
+
+	return lessons
+}
+
+func (p *Parser) getScheduleForGroup(group models.Group, weekNum int) {
+	var targetLink = fmt.Sprintf(
+		"https://rasp.rea.ru/Schedule/ScheduleCard?selection=%s&weekNum=%v&catfilter=0",
+		strings.ToLower(group.Name),
+		weekNum,
+	)
+	resp, err := p.client.Get(targetLink)
+	if err != nil {
+		log.Printf("Ошибка при получении основной страницы расписания: %s", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Printf("Ошибка при парсинге основной страницы расписания: %s", err.Error())
+		return
+	}
+
+	var lessons []models.Lesson
+
+	doc.Find("[class*=slot]:not([class*=empty])").Each(
+		func(i int, s *goquery.Selection) {
+			v, e := s.Find("[onclick]").Attr("onclick")
+			if e {
+				date := v[21:31]
+				timeslot, err := strconv.Atoi(v[35:36])
+				if err != nil {
+					//logrus
+					return
+				}
+				lessons = append(lessons, p.getLesson(group, date, timeslot)...)
+			}
+		})
+
+	p.storage.SaveLessons(context.TODO(), lessons)
 }
 
 func (p *Parser) GetSchedule() {
 	_, weekNum := time.Now().ISOWeek()
-	groups := p.storage.GetAllGroups()
+	groups := p.storage.GetAllGroups(context.TODO())
 	for _, group := range groups {
-		p.getScheduleForGroup(group.Name, weekNum)
-		p.getScheduleForGroup(group.Name, weekNum%53+1)
-		p.getScheduleForGroup(group.Name, weekNum%53+2)
+		p.getScheduleForGroup(group, weekNum+20)
+		p.getScheduleForGroup(group, (weekNum+20)%53+1)
+		p.getScheduleForGroup(group, (weekNum+20)%53+2)
 	}
 }
--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
+import (
+	"better-rasp/internal/parser"
+	"better-rasp/internal/storage"
+
+	"github.com/sirupsen/logrus"
+)
+
 func main() {
 	//Подключение storage
 	//Запуск и настройка (время запуска) парсеров
 	//Запуск сервера
+	cfg := storage.Config{
+		Host:     "localhost",
+		Port:     5432,
+		User:     "postgres",
+		Password: "postgres",
+		DBName:   "rasp",
+	}
+	logger := logrus.New()
+	storage := storage.New(cfg, logger)
+	parser := parser.New(&storage)
+	parser.Start()
 }
